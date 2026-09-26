d6873940c906a52bcc113fe5a5d2ea4a2223ecc8
BenDuske <benduske1979@gmail.com>
2026-09-26 09:39:11 -0500

    Fix map tiles: use Esri World Street Map (no API key required)

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -23,8 +23,8 @@ export default function MapView({ incidents, selectedId, onSelect }) {
       style={{ height: "100%", width: "100%", borderRadius: "8px" }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='Tiles &copy; Esri'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
       />
       <FlyToSelected incident={selected} />
 
